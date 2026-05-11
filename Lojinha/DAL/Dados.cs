d94138179cc66c4746f219e3a0ff8040de33dda3
RonieleMonteiro <roniele.bio@gmail.com>
2026-05-11 17:50:12 -0400

    StringConexao

---
+++ Lojinha/DAL/Dados.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lojinha.DAL
+{
+    public class Dados
+    {
+        public static string StringConexao
+        {
+            get 
+            {
+                return "Data Source=DESKTOP-PHMQ9A5\\MSSQLSERVER02;Initial Catalog=dblojinha;Integrated Security=True;Encrypt=False;Trust Server Certificate=True";
+
+            }
+
+
+
+        }
+            
+        
+
+    }
+}
