2af8986ff2730be5d339e0ee39ce87a4f7173145
RonieleMonteiro <roniele.bio@gmail.com>
2026-05-12 15:14:07 -0400

    Incluir Cliente

--- Lojinha/DAL/ClientesDAL.cs
+++ Lojinha/DAL/ClientesDAL.cs
@@ -1,13 +1,66 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Lojinha.Modelos;
-
+using System.Data
 namespace Lojinha.DAL
 {
     public class ClientesDAL
     {
+		public void Incluir (ClienteInformation cliente)
+		{
+        //Conexão com o banco de dados
+        SqlConnection cn = new SqlConnection();
+			try
+			{
+				SqlCommand cmd = new SqlCommand();
+				cmd.Connection = cn;
+				cmd.CommandType = CommandType.StoredProcedure;
+				cmd.CommandText = "insere_cliente";
+				//Paramentros da Stored Procedure
+				SqlParameter pcodigo = new SqlParameter("@codigo", SqlDbType.Int);
+				pcodigo.Direction = ParameterDirection.Output;
+				cmd.Parameters.Add(pcodigo);
+
+				SqlParameter pnome = new SqlParameter("@nome", SqlDbType.VarChar, 100);
+				pnome.Value = cliente.Nome;
+				cmd.Parameters.Add(pnome);
+
+				SqlParameter pemail = new SqlParameter("@email", SqlDbType.VarChar, 100);
+				pemail.Value = cliente.Email;
+				cmd.Parameters.Add(pemail);
+
+				SqlParameter ptelefone = new SqlParameter("@telefone", SqlDbType.VarChar, 100);
+				ptelefone.Value = cliente.Telefone;
+				cmd.Parameters.Add(ptelefone);
+
+				cn.Open();
+				cmd.ExecuteNonQuery();
+
+				cliente.Codigo = (Int32)cmd.Parameters["@codigo"].Value;
+
+
+			}
+			catch (SqlException ex)
+			{
+
+				throw new Exception("Erro ao acessar o banco de dados." + ex.Number);
+			}
+			catch 
+			{
+                throw new Exception("Erro ao acessar o banco de dados.");
+
+            }
+            finally
+            {
+			cn.Close();
+			}
+			
+
+
+		}
     }
 }
